365112ee542e91629fed6284fb140a113a017e9c
Rodrigo <rodrigo.martinez@escrol.es>
2026-02-23 17:54:30 +0100

    Modify some xml answers to avoid deserialization errors

--- src/plugin.validation.dns.arsys/ArsysClient/CustomTextMessageEncoder.cs
+++ src/plugin.validation.dns.arsys/ArsysClient/CustomTextMessageEncoder.cs
@@ -6,6 +6,7 @@ using System;
 using System.IO;
 using System.ServiceModel.Channels;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Xml;
 
 namespace PKISharp.WACS.Plugins.ValidationPlugins.ArsysClient
@@ -62,8 +63,56 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.ArsysClient
 
         public override Message ReadMessage(Stream stream, int maxSizeOfHeaders, string contentType)
         {
-            XmlReader reader = XmlReader.Create(stream);
-            return Message.CreateMessage(reader, maxSizeOfHeaders, this.MessageVersion);
+            /*
+             * Code to modify raw XML response.
+             * API says some vales cant be null, but in some scenarios it returns a nil value and this breaks the serializer.
+             * So we modify the XML to remove the nil, and add a false as the bool value
+             * Here you can attach a debuger stop point to see the raw XML
+             */
+            
+            string xml;
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                xml = reader.ReadToEnd();
+            }
+
+            // Uncomment if neccesary
+            //Console.WriteLine("----- Original Arsys XML -----");
+            //Console.WriteLine(xml);
+            //Console.WriteLine("-------------------------");
+            string pattern = @"<(?<name>\w+)([^>]*)\s+xsi:nil\s*=\s*""true""([^>]*)/?>";
+
+            xml = Regex.Replace(
+                xml,
+                pattern,
+                m =>
+                {
+                    string name = m.Groups["name"].Value;
+                    string allAttrs = m.Groups[1].Value + m.Groups[2].Value;
+
+                    // remove xsi:nil
+                    allAttrs = Regex.Replace(allAttrs, @"\s*xsi:nil\s*=\s*""true""", "").Trim();
+
+                    // remove / at end if exists
+                    allAttrs = allAttrs.TrimEnd('/');
+
+                    // rebuild node
+                    return $"<{name}{(allAttrs.Length > 0 ? " " + allAttrs : "")}>false</{name}>";
+                },
+                RegexOptions.IgnoreCase | RegexOptions.Multiline
+            );
+
+            // Uncomment if neccesary
+            //Console.WriteLine("----- Modified Arsys XML -----");
+            //Console.WriteLine(xml);
+            //Console.WriteLine("-------------------------");
+
+            var bytes = Encoding.UTF8.GetBytes(xml);
+            var newStream = new MemoryStream(bytes);
+
+            var xmlReader = XmlReader.Create(newStream);
+
+            return Message.CreateMessage(xmlReader, maxSizeOfHeaders, this.MessageVersion);
         }
 
         public override ArraySegment<byte> WriteMessage(Message message, int maxMessageSize, BufferManager bufferManager, int messageOffset)
